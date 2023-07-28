5f32b59eb2c2c9b10f7ea2c9b606f04e1a82f1de
lhunerberg <140844382+lhunerberg@users.noreply.github.com>
2023-07-28 14:31:29 -0700

    Update streamlit_app.py
    Spelling error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents\' New Healthy Diner')
-streamlit.header('Breankfast Favorites')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach, and Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
